189e5a3176a11d05d9cd066859b5e54b5431b5d3
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-09-07 16:24:08 +0530

    Authentication and Authorization | Displaying the Current User

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
+
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
+  user: firebase.User;
 
   constructor(private afAuth: AngularFireAuth) {
-      afAuth.authState.subscribe(x => console.log(x));
+      afAuth.authState.subscribe(user => this.user = user);
    }
 
   logout() {
