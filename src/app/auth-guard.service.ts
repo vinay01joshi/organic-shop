7f5bbb77b58589d6353db44b7235c8921611a18a
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-09-08 08:15:16 +0530

    Authentication and Authorization | Redirecting after Logging In

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from "@angular/router";
+import { CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 import { AuthService } from "./auth.service";
 import 'rxjs/add/operator/map';
 
@@ -8,11 +8,11 @@ export class AuthGurad implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
 
-  canActivate() {
+  canActivate(router, state: RouterStateSnapshot) {
     return this.auth.user$.map(user => {
       if(user) return true;
 
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
       return false;
 
     })
