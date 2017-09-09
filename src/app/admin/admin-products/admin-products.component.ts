d851a6a867e6a3ed3107c9383ec302d375e6544f
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-09-09 13:40:02 +0530

    Product Management Module | Feeding the Data Table

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,34 +1,58 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ProductService } from '../../product.service';
 import { Subscription } from 'rxjs/Subscription';
+
 import { Product } from '../../models/product';
+import { ProductService } from '../../product.service';
+import { DataTableResource } from 'angular-4-data-table';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit , OnDestroy {
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  itemCount: number;
   products: Product[];
   filteredProducts: any[];
   subscription: Subscription;
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
 
   constructor(private productService: ProductService) {
     this.subscription = this.productService.getAll()
-      .subscribe(products => this.filteredProducts = this.products = products);
-   }
+      .subscribe(products => {
+        this.filteredProducts = this.products = products;
+
+        this.initializeTable(products);
+      });
+  }
+
+  private initializeTable(products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({ offset: 10 })
+      .then(items => this.items = items);
+    this.tableResource.count()
+      .then(count => this.itemCount = count);
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+  reloadItems(params) {
+    if(!this.tableResource) return;
+
+    this.tableResource.query(params)
+    .then(items => this.items = items);
+  }
 
   ngOnInit() {
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
-  }
-
-  filter(query: string) {
-    this.filteredProducts = (query) ? 
-      this.products.filter( p => p.title.toLowerCase().includes(query.toLowerCase())) : 
-      this.products;
   }
 
 }
