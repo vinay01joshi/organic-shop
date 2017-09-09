bb1b25fa384eb35953a0d143e611f184e73f9c61
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-09-09 13:14:41 +0530

    Product Management Module | Adding data table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CustomFormsModule } from 'ng2-validation';
+import { DataTableModule } from 'angular-4-data-table';
+
 // Project Modues
 import { AppComponent } from './app.component';
 import { environment } from "../environments/environment";
@@ -49,6 +51,7 @@ import { ProductService } from './product.service';
     BrowserModule,
     FormsModule,
     CustomFormsModule,
+    DataTableModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
