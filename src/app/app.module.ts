6d11355b52c6c931df684d465405214880cdd3c5
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-09-09 11:38:34 +0530

    Product Management Module | Adding Custom Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { CustomFormsModule } from 'ng2-validation';
 // Project Modues
 import { AppComponent } from './app.component';
 import { environment } from "../environments/environment";
@@ -48,10 +48,11 @@ import { ProductService } from './product.service';
   imports: [
     BrowserModule,
     FormsModule,
+    CustomFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
-    NgbModule.forRoot(),
+    NgbModule.forRoot(),    
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'login', component: LoginComponent },
