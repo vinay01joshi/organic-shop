5caef646854b05c39296606cfdc46ada7540df54
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-09-11 08:42:05 +0530

    Shopping Cart Module | Refactoring the addToCart Method

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -10,11 +10,14 @@ export class ShoppingCartService {
 
   async addToCart(product: Product) {
      let cartId = await this.getOrCreateCartId();
-     let item$ =this.db.object('/shopping-carts/' + cartId + '/items/' + product.$key);
+     let item$ = this.getItem(cartId, product.$key);
      item$.take(1).subscribe(item => {
-      if(item.$exists()) item$.update({quantity: item.quantity +1});
-      else item$.set({product: product , quantity: 1});
+      item$.update({ product: product, quantity: (item.quantity || 0) +1 });
      });
+  }
+
+  private getItem(cartId:string , productId: string){
+    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
   }
 
   private create() {
