ae7af8dd23d9baccb3386cf070db2c179164da98
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-09-11 21:52:38 +0530

    Shopping Cart Module | Better Object Initialization

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -5,13 +5,11 @@ import { Product } from "./product";
 export class ShoppingCart {
     items: ShoppingCartItem[] = [];
     constructor(public itemsMap: {[productId: string]: ShoppingCartItem}) {  
-        this.itemsMap = itemsMap || {};      
+        this.itemsMap = itemsMap || {};
+
         for(let productId in itemsMap){
-            let item = itemsMap[productId];
-            let x = new ShoppingCartItem();
-            Object.assign(x, item);
-            x.$key = productId;
-            this.items.push(x);
+            let item = itemsMap[productId];         
+            this.items.push(new ShoppingCartItem({...item, $key: productId }));
         }
     }
 
