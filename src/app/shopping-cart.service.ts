8ee96abd3297e51a3401b514eea4bd2948785d74
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-09-11 21:46:01 +0530

    Shopping Cart Module | Flattening Shopping Cart Item Objects

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -12,18 +12,23 @@ export class ShoppingCartService {
   constructor(private db: AngularFireDatabase) { }
 
   async addToCart(product: Product) {
-    this.updateItemQunatity(product, 1);
+    this.updateItem(product, 1);
   }
 
   async removeFromCart(product: Product) {
-    this.updateItemQunatity(product, -1);
+    this.updateItem(product, -1);
   }
 
-  private async updateItemQunatity(product: Product, change: number) {
+  private async updateItem(product: Product, change: number) {
     let cartId = await this.getOrCreateCartId();
     let item$ = this.getItem(cartId, product.$key);
     item$.take(1).subscribe(item => {
-     item$.update({ product: product, quantity: (item.quantity || 0) + change });
+      item$.update({ 
+        title: product.title ,
+        imageUrl: product.imageUrl,
+        price: product.price,      
+        quantity: (item.quantity || 0) + change 
+      });
     });
   }
 
