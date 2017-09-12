1fbb819c3523fe6bc642c69b169944a1b9c4d2ea
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-09-12 19:00:06 +0530

    Check Out Module | Displaying the Orders

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../../order.service';
 
 @Component({
   selector: 'app-admin-orders',
   templateUrl: './admin-orders.component.html',
   styleUrls: ['./admin-orders.component.css']
 })
-export class AdminOrdersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class AdminOrdersComponent {
+  orders$;
+  
+    constructor(private orderService: OrderService) { 
+      this.orders$ = orderService.getOrders();
+    }
 
 }
--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../order.service';
+import { AuthService } from '../auth.service';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-my-orders',
   templateUrl: './my-orders.component.html',
   styleUrls: ['./my-orders.component.css']
 })
-export class MyOrdersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class MyOrdersComponent {
+  orders$;
+  constructor(orderService: OrderService, authService: AuthService) { 
+    this.orders$ = authService.user$.switchMap(u => orderService.getOrdersByUser(u.uid));
   }
-
 }
