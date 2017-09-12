c1f7ccf9fcefae3ae09ffd7a4046568bc73050f9
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-09-12 08:27:47 +0530

    Checkout Module | Refactoring: Extract an Order Model

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -4,6 +4,7 @@ import { ShoppingCart } from '../models/shopping-cart';
 import { Subscription } from 'rxjs/Subscription';
 import { OrderService } from '../order.service';
 import { AuthService } from '../auth.service';
+import { Order } from '../models/order';
 
 @Component({
   selector: 'app-check-out',
@@ -30,23 +31,7 @@ export class CheckOutComponent implements OnInit,OnDestroy {
   }
 
   placeOrder() {
-    let order = {
-      userId: this.userId,
-      datePlaced: new Date().getTime(),
-      shipping: this.shipping,
-      items: this.cart.items.map( i=> {
-        return {
-          product: {
-            title: i.title,
-            imageUrl: i.imageUrl,
-            price: i.price
-          },
-          quantity: i.quantity,
-          totoalPrice: i.totalPrice
-        }
-      })
-    };
-
+    let order = new Order(this.userId, this.shipping, this.cart);
     this.oderService.storeOrder(order);
   }
   
